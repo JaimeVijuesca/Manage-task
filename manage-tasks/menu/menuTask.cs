63350139cecdcea239998b04110990c098d92f7f
Jaime <jaimevijuescafraca@gmail.com>
2026-01-02 14:10:38 +0100

    Create the structure to the menu task

--- manage-tasks/menu/menuTask.cs
+++ manage-tasks/menu/menuTask.cs
@@ -1,12 +1,36 @@
 
 namespace GestorTareas.Menu;
+
 public class menuTask
 {
 
     public void Run()
     {
-        Console.WriteLine(""); //Imprime by console
-       
+
+        Console.WriteLine("1.See Tasks"); //Imprime by console
+        Console.WriteLine("2.Create Task");
+        Console.WriteLine("3.Delete Task");
+
+        if (!int.TryParse(Console.ReadLine(), out int result))
+        {
+            Console.WriteLine("Invalid input. Please input a number");
+        }
+        switch (result)
+        {
+            case 1:
+                Console.WriteLine("Here see the tasks");
+                break;
+            case 2:
+                Console.WriteLine("Here create the task");
+                break;
+            case 3:
+                Console.WriteLine("Here delete the task");
+                break;
+            default:
+                Console.WriteLine("Default");
+                break;
+        }
+
     }
-    
+
 }
