a3a2aa3becf72458a3cdae26c9d3eca917993176
Jaime <jaimevijuescafraca@gmail.com>
2026-01-02 17:41:36 +0100

    Implement the interface in the UserServices

---
+++ manage-tasks/Services/UserService.cs
@@ -0,0 +1,27 @@
+public class UserServices : IUserService
+{
+    public void createUser(string username, string password, string repeatingPassword)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void deleteUser(string username, string password, string repeatingPassword)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void updateUserName(string username, string password, string repeatingPassword, string newName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void updateUserPassword(string username, string password, string repeatingPassword, string newPassword)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool userExists(string username)
+    {
+        throw new NotImplementedException();
+    }
+}
